2f7c1069e2c1a1b96bb1c21b546ece9af082a0dc
ejkim9090 <ej.kh.kim@gmail.com>
2023-09-05 11:08:06 +0900

    react - 첫 프로젝트

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ImportComponent from './R003_ImportComponent';
+import LifecycleEx from './R004_LifecycleEx';
+import $ from 'jquery';
+
+function App() {
+  return (
+    <div className="App">
+      <ImportComponent></ImportComponent>
+      <h1 >Hello React</h1>
+      <LifecycleEx></LifecycleEx>
+    </div>
+  );
+}
+
+export default App;
