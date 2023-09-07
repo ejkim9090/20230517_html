489054e9ff4eed5271269afa1a1d201ca213436e
ejkim9090 <ej.kh.kim@gmail.com>
2023-09-07 12:48:21 +0900

    react - insert - list 확인 useState

--- react/fruitshop/src/App.js
+++ react/fruitshop/src/App.js
@@ -1,25 +1,53 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useState } from 'react';
 /*
 url mapping 페이지 구성
 나타낼 속성들을 모아서 entity 별로 묶기
 이름, 가격, 단위 == 과일
+TODO delete - 목록화면에서 버튼 만들어서 1개 삭제
+TODO info - id 활용해서 보기
 */
 
 function App() {
-  const [a1, a2] = useState([10, 20, 30]);  // a1 저장공간생성
-  //fruit 이름의 저장공간 생성 선언
+  //const [a1, a2] = useState([10, 20, 30]);  // a1 저장공간생성
+  //const [a,seta] = useState();
+
+  // fruitList 이름의 저장공간 생성 선언
+  const [fruitList, setFruitList] = useState(
+    [
+      {
+        id : '1',
+        name : '기본이름1',
+        price : '기본값1',
+        count : '기본단위1'
+      },
+      {
+        id : '2',
+        name : '기본이름2',
+        price : '기본값2',
+        count : '기본단위2'
+      },
+    ]
+  );
+
+  // newFruit 이름의 저장공간 생성 선언
   const [newFruit, setNewFruit] = useState( 
     {
-      name : '',
-      price : '',
-      count : ''
+      id : '',
+      name : '기본이름',
+      price : '기본값',
+      count : '기본단위'
     }
   );
   const insertHandler = () => {
     console.log("insertHandler 함수실행");
     console.log(newFruit); 
+    // TODO id 중복값 없게
+    // TODO 빈칸 없게
+    // 기존 fruitList에 동일한 것이 없다면 추가
+    setFruitList([...fruitList, newFruit]);
+    console.log(fruitList);
   }
   const onChangeInput = (event) => {
     console.log(event.target);
@@ -44,7 +72,7 @@ function App() {
         </nav>
         <div>
           <form>
-            <div><span>이름:</span><input type="text"  onChange={onChangeInput}  value={newFruit.name} name="name"   ></input></div>
+            <div><span>이름:</span><input type="text"  onChange={onChangeInput}  value={newFruit.name}  name="name"   ></input></div>
             <div><span>가격:</span><input type="text"  onChange={onChangeInput}  value={newFruit.price} name="price"></input></div>
             <div><span>단위:</span><input type="text"  onChange={onChangeInput}  value={newFruit.count} name="count"></input></div>
           </form>
@@ -53,7 +81,6 @@ function App() {
       </>
     );
   }
-
 
 
   const List = (props) => {
@@ -77,6 +104,29 @@ function App() {
             <li><Link to="/">홈</Link></li>
           </ul>
         </nav>
+        <table>
+          <thead>
+            <tr>
+              <td>이름</td>
+              <td>가격</td>
+              <td>단위</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              fruitList.map( (fruit)=> { 
+                console.log(fruit);
+                return (
+                  <tr>
+                    <td>{fruit.name}</td>
+                    <td>{fruit.price}</td>
+                    <td>{fruit.count}</td>
+                  </tr>
+                )
+              } )
+            }
+          </tbody>
+        </table>
       </>
     );
   }
