0a98d96bc58b493e89b26e5d1e1fb84fe097cdbe
ejkim9090 <ej.kh.kim@gmail.com>
2023-09-07 17:16:49 +0900

    react - 과일가게 단계별 코딩 - 2 /insert /list

--- react/fruiteshop_stepbystep/src/App.js
+++ react/fruiteshop_stepbystep/src/App.js
@@ -4,15 +4,26 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   const Home = ()=>{
     return (
-      <h1>Home</h1>
+      <h1>홈</h1>
     );
   }
-
+  const List = ()=>{
+    return (
+      <h1>과일목록</h1>
+    );
+  }
+  const Insert = ()=>{
+    return (
+      <h1>과일추가</h1>
+    );
+  }
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path='/' element={<Home></Home>} />
+          <Route path='/list' element={<List></List>} />
+          <Route path='/insert' element={<Insert></Insert>} />
         </Routes>
       </div>
     </BrowserRouter>
