79946b3f8d07999de78d5d71d25e92145eeb965a
ejkim9090 <ej.kh.kim@gmail.com>
2023-09-07 17:25:58 +0900

    react - 과일가게 단계별 코딩 - 4 임의화면 /insert /list

--- react/fruiteshop_stepbystep/src/App.js
+++ react/fruiteshop_stepbystep/src/App.js
@@ -25,6 +25,29 @@ function App() {
             <li><Link to="/">홈</Link></li>
           </ul>
         </nav>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>이름</th>
+                <th>가격</th>
+                <th>수량</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>이름1</td>
+                <td>가격1</td>
+                <td>수량1</td>
+              </tr>
+              <tr>
+                <td>이름2</td>
+                <td>가격2</td>
+                <td>수량2</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </>
     );
   }
@@ -38,6 +61,12 @@ function App() {
             <li><Link to="/">홈</Link></li>
           </ul>
         </nav>
+        <div>
+          <div><span>이름</span><input></input></div>
+          <div><span>가격</span><input></input></div>
+          <div><span>수량</span><input></input></div>
+          <div><button>등록</button></div>
+        </div>
       </>
     );
   }
